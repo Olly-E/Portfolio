1c1d586e56b818f39627b22314b6dc64d3fb9d44
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-04-02 12:27:26 +0100

    updating expeience css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -13,56 +13,56 @@ const Experience = () => {
           <h3>Frontend Development</h3>
           <div className='experience__content'>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
                 <h4>HTML</h4>
                 <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>CSS</h4>
               <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>JavaScript</h4>
               <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>Tailwind</h4>
               <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>Bootstrap</h4>
               <small className="text-light">Experienced</small>
               </div>
             </article>
               <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>React</h4>
               <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon"/>
               <div>
               <h4>Photoshop</h4>
               <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
               <h4>Coreldraw</h4>
               <small className="text-light">Intermediate</small>
