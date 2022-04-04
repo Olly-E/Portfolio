5911d5fdc0ad6781e255ce99ed5bb285018891c0
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-04-04 21:45:54 +0100

    adding other sections like the contact and the testimonials

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,73 +7,72 @@ import IMG4 from '../../assets/portfolio4.jpg';
 import IMG5 from '../../assets/portfolio5.png';
 import IMG6 from '../../assets/portfolio6.jpg';
 
+const data = [
+  {
+    id:1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard and financial visualization',
+    github: 'http://github.com',
+    demo: '#'
+  },
+  {
+    id:2,
+    image: IMG2,
+    title: 'Charts template and infographics in figma',
+    github: 'http://github.com',
+    demo: '#'
+  },
+  {
+    id:3,
+    image: IMG3,
+    title: 'Figma design UI kit for data design and web apps',
+    github: 'http://github.com',
+    demo: '#'
+  },
+  {
+    id:4,
+    image: IMG4,
+    title: 'Maintaining tasks and tracking progress',
+    github: 'http://github.com',
+    demo: '#'
+  },
+  {
+    id:5,
+    image: IMG5,
+    title: 'Charts template and infographics in figma',
+    github: 'http://github.com',
+    demo: '#'
+  },
+  {
+    id:6,
+    image: IMG6,
+    title: 'Charts template and infographics in figma',
+    github: 'http://github.com',
+    demo: '#'
+  }
+]
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
-      <div className="portfolio__container"> 
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
-      <article className='portfolio__item'>
-        <div className="portfolio__item-image">
-          <img src={IMG1} alt="" />
-        </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target="_blank">Live Demo</a>
-          </div>
-      </article>
 
+      <div className="portfolio__container">
+        {data.map(({id, image, title, github, demo}) => {
+          return (
+            <article key={id} className='portfolio__item'>
+        <div className="portfolio__item-image">
+          <img src={image} alt={title} />
+        </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+            <a href={github} className='btn'>Github</a>
+            <a href={demo} className='btn btn-primary' target="_blank">Live Demo</a>
+          </div>
+      </article>
+          )
+        })}
       </div>
     </section>
   )
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,24 @@
-import './contact.css'
+import './contact.css';
+import {MdOutlineMail} from 'react-icons/md'
 
 const Contact = () => {
   return (
     <section id="contact">
-      Contact
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail />
+            <h4>Email</h4>
+            <h5>akinsnuel@gmail.com</h5>
+            <a href="mailto:akinsnuel@gmail.com">Send a message</a>
+          </article>
+        </div>
+        {/* End of contact options */}
+        <form action=""></form>
+      </div>
     </section>
   )
 }
