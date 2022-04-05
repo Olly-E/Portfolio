e01254ac432817a3381f9cfbade3722bfe6c2290
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-04-05 05:52:21 +0100

    finished the mail api and the contact section genraly

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,13 +2,22 @@ import './contact.css';
 import {MdOutlineMail} from 'react-icons/md';
 import {RiMessengerLine} from 'react-icons/ri';
 import {BsWhatsapp} from 'react-icons/bs';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fwevu46', "template_cnxhe8b", form.current, 
+      "_Ykk3DhR2U2VEPvn2")
+      e.target.reset();
+  };
   return (
     <section id="contact">
       <h5>Get in touch</h5>
       <h2>Contact Me</h2>
-
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
@@ -31,7 +40,7 @@ const Contact = () => {
           </article>
         </div>
         {/* End of contact options */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required/>
           <input type="email" name="email" placeholder="email" />
           <textarea name="message" placeholder="Your message..." rows="7" required></textarea>
