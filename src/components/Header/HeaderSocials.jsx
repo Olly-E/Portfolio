cc6f429ec67e7978097318f7fcdc6e3a8c3f3b57
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-28 08:20:21 -0700

    adjust url for socials

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -8,12 +8,12 @@ import { headerIcons1, headerIcons2, headerIcons3 } from '../Animation';
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <motion.a href="https://linkedin.com" target="_blank"
+      <motion.a href="https://www.linkedin.com/in/emmanuelakinsiku/" target="_blank"
         variants={headerIcons3}
         initial="hidden"
         animate="show"
       ><BsLinkedin /></motion.a>
-      <motion.a href="https://github.com" target="_blank"
+      <motion.a href="https://github.com/Olly-E" target="_blank"
         variants={headerIcons2}
         initial="hidden"
         animate="show"
